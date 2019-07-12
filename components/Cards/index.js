defa99af79082d32f01da369220cc9543cf67508
Jade E Lopez <38263897+jadeli1720@users.noreply.github.com>
2019-07-12 10:06:23 -0600

    Modified components/Cards/index.js: Grabbed axios get request to study the data

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,6 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+const promise = axios.get(`https://lambda-times-backend.herokuapp.com/articles`)
+console.log(promise)
