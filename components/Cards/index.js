fb660d70c9045d5dadf2c5f9483bd0b4dc39e6ac
Jade E Lopez <38263897+jadeli1720@users.noreply.github.com>
2019-07-12 10:23:33 -0600

    Modified components/Cards/index.js: created, appended, and ass styles in createCard function. Appended (tested) the function to the cards-container.

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -18,5 +18,36 @@
 //
 // Create a card for each of the articles and add the card to the DOM.
 
+const cards = document.querySelector('.cards-container')
+console.log(cards)
+
+cards.appendChild(createCard())
+
 const promise = axios.get(`https://lambda-times-backend.herokuapp.com/articles`)
 console.log(promise)
+
+function createCard(data) {
+    //create elements
+    const card = document.createElement('div'),//parent
+          headline = document.createElement('div'),//child 
+          author = document.createElement('div'),//child 
+          imgContainer = document.createElement('div'),//grandchild 
+          img = document.createElement('img'),//great-grandchild
+          authorName = document.createElement('span');//grandchild  
+
+    //append elements
+    card.appendChild(headline)
+    card.appendChild(author)
+    author.appendChild(imgContainer)
+    imgContainer.appendChild(img)
+    author.appendChild(authorName)
+
+    //create styles
+    card.classList.add('card');
+    headline.classList.add('headline');
+    author.classList.add('author');
+    imgContainer.classList.add('img-container');
+    
+    //event listener?
+    return card;
+}
