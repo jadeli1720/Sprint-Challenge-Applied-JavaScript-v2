b6df326b604052e1a078ee0e636fc18936e616ec
Jade E Lopez <38263897+jadeli1720@users.noreply.github.com>
2019-07-14 23:13:37 -0600

    Images change when both buttons are clicked and loop through properly. Need to find a better way to code right button though.

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -17,22 +17,18 @@
     <div class="right-button"> > </div>
   </div>
 */
+
 const imageArray = [
   '/assets/carousel/mountains.jpeg',
   './assets/carousel/computer.jpeg',
   './assets/carousel/trees.jpeg',
   './assets/carousel/turntable.jpeg',
-]
+];
 // console.log(imageArray)
-
-imageArray.forEach(image => {
-  // console.log(image)
-});
 
 const carouselContainer = document.querySelector('.carousel-container')
 carouselContainer.appendChild(createCarousel());
 // console.log(carouselContainer)
-
 
 
 function createCarousel () {
@@ -58,33 +54,37 @@ function createCarousel () {
   //fill with text content
   leftButton.textContent = '<';
   rightButton.textContent = '>';
-  img.src = imageArray[index];//sets the src value to the values inside the array
-  img.style.display= 'block'//This displays them....need to iterate over image to display different src
+  img.src = imageArray[index];//sets the src property to the values inside the array
+  img.style.display= 'block'//This displays the first image before click event fires
 
  
   //event listeners
   leftButton.addEventListener('click', event => {
-    // console.log('Left button clicked', event.target)
+    console.log('Left button clicked', event.target)
     index --;
-    if (index < 0) {
+    if (index <= 0) {
        index = imageArray.length - 1;
-    }
+    } 
+
     img.src = imageArray[index];
-     console.log("clicked left",index);
-     carousel.appendChild(img);
+    carousel.appendChild(img);
+    console.log("Clicked left. We are at index:",index);//This shows me at what index carousel is at when the left button is clicked
   });
 
   rightButton.addEventListener('click', event => {
-    // index ++
-    if (index >= 0) {
-      index = imageArray.length + 1;
-    } else {
-      index ++
+    if (index >= imageArray[index] ) {
+      index = imageArray.length + 1 ;
+
+    } else if( index === 3) {
+      index = 0;
+      
+    } else{
+      index ++;
     }
+
     img.src = imageArray[index];
-    img.style.display = 'block'
     carousel.appendChild(img);
-    console.log("clicked right",index);
+    console.log("Clicked right. We are at index:",index);//This shows me at what index carousel is at when the right button is clicked
   });
 
 
